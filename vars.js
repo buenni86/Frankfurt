8d22371479757196eb260e4c161c19228c6626c2
buenni86 <12584248+buenni86@users.noreply.github.com>
2022-05-13 14:05:45 +0000

    Deploying to gh-pages from @ buenni86/Frankfurt@8c41cc4fa911e25dcedae4c7d57e68a5b88e21b6 🚀

---
+++ vars.js
@@ -0,0 +1,4 @@
+export var programMsg = "Mehr Informationen zu DB WorkAdventure?";
+export var urlProgram = "https://db.de/workadventure";
+export var programZone = "program";
+export var programPopUp = "popUpProgram";
