43dc9faefe77ea63d7093f04b7a4f9452f8ccbac
Alexis Faizeau <a.faizeau@workadventu.re>
2022-06-20 15:10:31 +0200

    xFixes after updated starter kit

--- train1_script.js
+++ train1_script.js
@@ -1,18 +1,22 @@
-import { } from "https://unpkg.com/@workadventure/scripting-api-extra@^1";
+import { } from "@workadventure/scripting-api-extra";
 import {track1Map, setTrackContent, refreshSigns } from "./sign_script.js";
 import {openPopupWithWebsiteYesNo, closePopupWithWebsite } from "./popUp_script.js";
 import * as vars from "./vars.js";
 
-const sign1a = await WA.room.website.get("sign1a");
-const sign1b = await WA.room.website.get("sign1b");
+WA.onInit(async () => {
+    const sign1a = await WA.room.website.get("sign1a");
+    const sign1b = await WA.room.website.get("sign1b");
 
-const signToTrackMap = new Map ([
-    [sign1a, track1Map],
-    [sign1b, track1Map]
-]);
+    const signToTrackMap = new Map ([
+        [sign1a, track1Map],
+        [sign1b, track1Map]
+    ]);
 
-setTrackContent(signToTrackMap);
-refreshSigns(signToTrackMap);
+    setTrackContent(signToTrackMap);
+    refreshSigns(signToTrackMap);
 
-WA.room.onEnterZone(vars.programZone, () => {openPopupWithWebsiteYesNo(vars.programPopUp, vars.programMsg, vars.urlProgram)})
-WA.room.onLeaveZone(vars.programZone, () => {closePopupWithWebsite()})
+    WA.room.onEnterZone(vars.programZone, () => {openPopupWithWebsiteYesNo(vars.programPopUp, vars.programMsg, vars.urlProgram)})
+    WA.room.onLeaveZone(vars.programZone, () => {closePopupWithWebsite()})
+});
+
+export {};
