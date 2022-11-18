2be143dcb4bbe5664e2ce4355b1f249edec0e4fe
Altin Zhuta <altin.zhuta@deutschebahn.com>
2022-11-18 13:09:03 +0100

    success story pop and pdf  added

--- train5_script.js
+++ train5_script.js
@@ -3,11 +3,21 @@ import { track5Map, setTrackContent, refreshSigns } from "./sign_script.js";
 import { openPopupWithWebsiteYesNo, closePopupWithWebsite } from "./popUp_script.js";
 import * as vars from "./vars.js";
 
+var currentPopup = undefined;
+
+function closePopUp(){
+    if (currentPopup !== undefined) {
+        currentPopup.close();
+        currentPopup = undefined;
+    }
+}
+
 WA.onInit(async () => {
     bootstrapExtra().then(() => {
         console.log('Scripting API Extra ready');
     }).catch(e => console.error(e));
-
+	
+	
     const sign5a = await WA.room.website.get("sign5a");
     const sign5b = await WA.room.website.get("sign5b");
 
@@ -18,9 +28,20 @@ WA.onInit(async () => {
 
     setTrackContent(signToTrackMap);
     refreshSigns(signToTrackMap);
-
-    WA.room.onEnterZone(vars.programZone, () => { openPopupWithWebsiteYesNo(vars.programPopUp, vars.programMsg, vars.urlProgram) })
-    WA.room.onLeaveZone(vars.programZone, () => { closePopupWithWebsite() })
+		
+    
 })
 
+WA.room.onEnterZone("program", () => { 
+	currentPopup =  WA.ui.openPopup("popUpProgram","Was. Ein. Event. Über 1.300 Techies aus dem gesamten Konzern kamen vom 10. bis 12. Mai 2022 auf der DB TechCon zusammen, um sich intensiv über das breite IT- und Technikspektrum der Deutschen Bahn auszutauschen - und mehr.\n\n All das komplett virtuell auf der riesigen DB TechCon Map in WorkAdventure.\nIn über 100 Beiträgen in Form von Vorträgen, Workshops und Diskussionsrunden wurden Best Practices und Fails ausgetauscht, Wissen geteilt, genetzwerkt, diskutiert, gewerkelt und nicht zuletzt:\ngemeinsam Spaß gehabt!",[
+        {
+            label: "OK",
+            callback: (popup => {
+                closePopUp();
+            })
+        }]); 
+		})
+    
+	
+	WA.room.onLeaveZone("program", () => { closePopUp() })
 export { };
